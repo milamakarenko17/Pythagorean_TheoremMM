24af609ee88412a5d535822a26ac4f53aed15e90
milamakarenko17 <145554532+milamakarenko17@users.noreply.github.com>
2024-01-11 23:23:43 +0200

    Add files via upload

--- README.md
+++ README.md
@@ -1,1 +1,9 @@
-# Pythagorean_TheoremMM
+# pythagorean-theorem-calculator
+
+Pythagorean theorem calculator using node.js
+
+Steps:
+
+1. npm i
+2. npm start
+3. open <http://localhost:3000/>
---
+++ main.js
@@ -0,0 +1,30 @@
+const express = require('express');
+const path = require('path');
+const inputHandler = require('./inputHandler');
+const calculatorRoutes = require('./calculatorRoutes');
+
+
+const app = express();
+const PORT = process.env.PORT || 3000;
+
+
+app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.urlencoded());
+app.use(express.json('public'));
+
+nunjuks.configure('views', {
+ autoescape: true,
+ express: app
+});
+
+app.get('/', (req, res) => {
+  res.sendFile(path.join(__dirname, 'public','index.html'));
+});
+
+app.use ('/calculate', calculatorRoutes);
+
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
+});
+
+inputHandler.startInput();
